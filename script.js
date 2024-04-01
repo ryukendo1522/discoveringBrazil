9dfcddf298292465461a0012af05a8e3a52dd373
Thlimass <thlima.silva.souza@gmail.com>
2024-04-01 18:33:24 -0300

    creating script.js file

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const description = document.querySelector(".map-content");
+
+/*Função responsável por ativar uma descrição flutuante */
+document.querySelectorAll('path').forEach((el) =>
+    el.addEventListener('mouseover', (event) => {
+        event.target.className = ("enabled");
+        description.classList.add("active");
+        description.innerHTML = event.target.id;
+    })
+
+);
+
+/*Função remove a descrição quando o mouse sai do elemento path*/
+document.querySelectorAll('path').forEach((el) =>
+    el.addEventListener("mouseout", () => {
+        description.classList.remove("active");
+    })
+);
+
+/*Função que segue o movimento do mouse */
+document.onmousemove = function (e) {
+    description.style.left = e.pageX + "px";
+    description.style.top = (e.pageY - 70) + "px";
+}
+
